a06dcf386e68b984cf0e0567dccb45dde1e33bb1
Akojuru Anthony <montana@Akojurus-MacBook-Pro.local>
2026-03-18 08:54:22 -0700

    update

--- script.js
+++ script.js
@@ -67,3 +67,15 @@ if (menuToggle && navMenu) {
       });
     }
   };
+
+   // Animation
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+        }
+    });
+}, { threshold: 0.1 }); // Triggers when 10% of the element is visible
+
+// Attach to everything with the 'reveal' class
+document.querySelectorAll('.reveal').forEach((el) => observer.observe(el));
